21b9350c852c2a5605b0a4c586ae7a88cf32df00
PetroGitH <144315410+PetroGitH@users.noreply.github.com>
2026-03-11 09:50:06 +0100

    Add files via upload
    Signed-off-by: PetroGitH <144315410+PetroGitH@users.noreply.github.com>

--- webA.js
+++ webA.js
@@ -4,3 +4,24 @@ Telegram.WebApp.MainButton
     .onClick(() => {
         Telegram.WebApp.sendData("ok");
     });
+
+Telegram.WebApp.sendData(JSON.stringify({
+    action: "COMMANDE",
+    panier: panier
+}))
+
+document.getElementById("addressForm").addEventListener("submit", function(e) {
+    e.preventDeflault();
+
+    const clientData ={
+        name: document.getElementById("name".value),
+        street: document.getElementById("street").value,
+        city: document.getElementById("city").value,
+        phone: document.getElementById("phone").value
+    };
+
+    // on save temporairement les données
+    localStorage.setItem("clientData", JSON.stringify(clientData));
+
+    window.location.href ="menu.html";
+});
